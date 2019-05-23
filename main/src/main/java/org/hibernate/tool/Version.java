14352d6813210b2725d3cc25cfe18296a13393ba
liuzhg <39865635@qq.com>
2019-05-23 11:52:40 +0800

    5.4.2.F01

--- main/src/main/java/org/hibernate/tool/Version.java
+++ main/src/main/java/org/hibernate/tool/Version.java
@@ -5,7 +5,7 @@ import org.apache.commons.logging.LogFactory;
 
 final public class Version {
 
-	public static final String VERSION = "5.4.2.Final";
+	public static final String VERSION = "5.4.2.F01";
 	
 	private static final Version instance = new Version();
 	
